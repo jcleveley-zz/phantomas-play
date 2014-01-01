690beff8f4447a32a8916e37dc60257a3dd7312d
John Cleveley <cleveley@gmail.com>
2014-01-01 21:12:14 +0000

    upload to s3

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
           indexPath : './output/frontPageDesktop/',
           raw       : [],
           url       : 'http://www.live.bbc.co.uk/news',
-          numberOfRuns : 3
+          numberOfRuns : 1
         }
       },
       frontPageMobile : {
@@ -16,12 +16,51 @@ module.exports = function(grunt) {
           indexPath : './output/frontPageMobile/',
           raw       : ['--viewport=[400]x[800]'],
           url       : 'http://www.live.bbc.co.uk/news',
-          numberOfRuns : 3
+          numberOfRuns : 1
         }
+      }
+    },
+    aws: grunt.file.readJSON('secret.json'),
+    s3: {
+      options: {
+        key: '<%= aws.key %>',
+        secret: '<%= aws.secret %>',
+        bucket: '<%= aws.bucket %>',
+        access: 'public-read'
+      },
+      dev: {
+        upload: [
+          {
+            src: 'output/frontPageMobile/index.html',
+            dest: 'frontPageMobile.html'
+          },
+          {
+            src: 'output/frontPageDesktop/index.html',
+            dest: 'frontPageDesktop.html'
+          },
+          {
+            src: 'output/*/data/*.json',
+            dest: 'data/'
+          },
+          {
+            src: 'output/frontPageMobile/public/scripts/*',
+            dest: 'public/scripts'
+          },
+          {
+            src: 'output/frontPageMobile/public/styles/*',
+            dest: 'public/styles'
+          },
+          {
+            src: 'index.html',
+            dest: 'index.html'
+          }
+        ]
       }
     }
   });
 
+  grunt.task.registerTask('default', ['phantomas', 's3']);
 
+  grunt.loadNpmTasks('grunt-s3');
   grunt.loadNpmTasks('grunt-phantomas');
 };
--- README.md
+++ README.md
@@ -10,4 +10,11 @@
 ## Grunt plugin issues
 
 - indexPath doesn't create nested folders
-- Useful to have a single graph on a page for dashboards
+- Useful to have a single graph or a page for dashboards
+
+
+## Phantomas issues
+
+``
+message: '2013-12-24 08:50:41.223 phantomjs[4004:507] *** WARNING: Method userSpaceScaleFactor in class NSView is deprecated on 10.7 and later. It should not be used in new applications. Use convertRectToBacking: instead. \n2013-12-24 08:50:42.610 phantomjs[4004:507] CoreText perfor ....
+```
